e632739fe66056a618198296adcbe443321be76b
Robert Scarlett <scarlettt10@msn.com>
2017-08-16 11:45:47 -0600

    fixed exit command

--- main.rb
+++ main.rb
@@ -1,47 +1,47 @@
-# def puts_git(cmd)
-#     puts `git #{cmd} -h`
-# end
-
-# def menu
-#     puts '1: Enter Git Command'
-#     puts '2: Exit'
-#     choice = gets.to_i
-#     case 
-#         when 1
-#             puts 'Enter a Git Command'
-#             puts_git(gets.strip)
-#         when 2
-#             puts 'Thanks for using the git command'
-#             exit
-#         else
-#             puts 'Invalid Choice!'
-#             menu
-#     end
-#     menu
-# end
-
-menu
-
 def puts_git(cmd)
     puts `git #{cmd} -h`
-  end
-  
-  def menu
+end
+
+def menu
     puts '1: Enter Git Command'
     puts '2: Exit'
     choice = gets.to_i
     case choice
-      when 1
-        puts 'Enter A Git Command'
-        puts_git(gets.strip)
-      when 2
-        puts 'Thanks for using the git command'
-        exit
-      else
-        puts 'Invalid Choice!'
-        menu
+        when 1
+            puts 'Enter a Git Command'
+            puts_git(gets.strip)
+        when 2
+            puts 'Thanks for using the git command'
+            exit
+        else
+            puts 'Invalid Choice!'
+            menu
     end
     menu
-  end
+end
+
+menu
+
+# def puts_git(cmd)
+#     puts `git #{cmd} -h`
+#   end
   
-  menu
+#   def menu
+#     puts '1: Enter Git Command'
+#     puts '2: Exit'
+#     choice = gets.to_i
+#     case choice
+#       when 1
+#         puts 'Enter A Git Command'
+#         puts_git(gets.strip)
+#       when 2
+#         puts 'Thanks for using the git command'
+#         exit
+#       else
+#         puts 'Invalid Choice!'
+#         menu
+#     end
+#     menu
+#   end
+  
+#   menu
